a4b7de175f30d9f9208d8f903f75161bd05d6170
GabrielNatPr0 <gabriel.contessoto.ribeiro@escola.pr.gov.br>
2026-03-16 09:54:57 -0300

    fix: isPalindrome

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class IsPalindrome {
+    private static boolean checkPalindrome(String num){
+        int left = 0;
+        int right = num.length() - 1;
+        while (left < right){
+            if(num.charAt(left) != num.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(int x){
+        if(x < 0){
+            return false;
+        }
+        String num = x + "";
+        return checkPalindrome(num);
+    }
+
+//    public static void main(String[] args) {
+//        int input = 121;
+//        if (isPalindrome(input)){
+//            System.out.println("\"" + input + "\" is a palindrome.");
+//        } else {
+//            System.out.println("\"" + input + "\" is not a palindrome.");
+//        }
+//    }
 }
